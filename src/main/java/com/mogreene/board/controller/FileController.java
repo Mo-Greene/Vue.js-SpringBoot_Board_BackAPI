1b2068ff61e5f089fc5c83a1d616ee77dbc9e71a
Mo-Greene <13blueboy13@daum.net>
2023-03-09 16:37:53 +0900

    파일 공통 api response 처리

--- src/main/java/com/mogreene/board/controller/FileController.java
+++ src/main/java/com/mogreene/board/controller/FileController.java
@@ -1,11 +1,13 @@
 package com.mogreene.board.controller;
 
+import com.mogreene.board.common.api.ApiResponseDTO;
 import com.mogreene.board.dto.FileDTO;
 import com.mogreene.board.service.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,14 +29,19 @@ public class FileController {
      * @return
      * @throws IOException
      */
-    // TODO: 2023/03/09 파일 다운로드 할땐 공통된 api 로 작성을 할 수 있을지 고민하자
+    // TODO: 2023/03/09 http 헤더를 처리하기 위해 이렇게 처리했는데 올바른 결과값으로 가는지는 잘 모르겠다. 포스트맨 확인x
     @GetMapping("/download/{fileNo}")
-    public ResponseEntity<Resource> fileDown(@PathVariable("fileNo") Long fileNo) throws IOException {
+    public ResponseEntity<ApiResponseDTO<Resource>> fileDown(@PathVariable("fileNo") Long fileNo) throws IOException {
 
         FileDTO fileDTO = fileService.downloadFile(fileNo);
 
         return ResponseEntity.ok().contentType(MediaType.parseMediaType("application/octet-stream"))
                 .header(HttpHeaders.CONTENT_DISPOSITION, fileDTO.getContentDisposition())
-                .body(fileDTO.getResource());
+                .body(ApiResponseDTO.<Resource>builder()
+                        .resultType(true)
+                        .httpStatus(HttpStatus.OK)
+                        .resultCode(HttpStatus.OK.value())
+                        .resultData(fileDTO.getResource())
+                        .build());
     }
 }
