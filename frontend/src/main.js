17bfe26425798a44299335ab9fe773306db70e32
Mo-Greene <13blueboy13@daum.net>
2023-03-10 19:09:28 +0900

    add: frontend App.vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import mixins from '@/mixins'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.mixin(mixins)
+app.mount('#app')
