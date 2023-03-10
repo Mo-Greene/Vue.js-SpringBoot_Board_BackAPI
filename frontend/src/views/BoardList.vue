17bf04b389d4cda4720a00ba0c189daf6a8c3dd2
Mo-Greene <13blueboy13@daum.net>
2023-03-10 20:06:49 +0900

    add: CorsConfig error: list for문이 계속 돌고 있음 아마도 boardList 객체 안에 데이터를 찾느라 그런듯함

---
+++ frontend/src/views/BoardList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Content</th>
+          <th>Writer</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="i" v-for="(board,i) in boardList">
+          <td>{{ board.resultData.boardTitle }}</td>
+          <td>{{ board.resultData.boardContent }}</td>
+          <td>{{ board.resultData.boardWriter }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      boardList: []
+    }
+  },
+  created () {
+    this.getBoardList()
+  },
+  methods: {
+    async getBoardList () {
+      this.boardList = await this.$api('http://localhost:8080/boards/list', 'get')
+      console.log(this.getBoardList())
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main/java/com/mogreene/board/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.mogreene.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CorsConfiguration
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3000);
+    }
+}
